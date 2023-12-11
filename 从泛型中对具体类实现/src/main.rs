d40a139303c54e5b113b8bf1b4130909bf86c85d
weykon <635883092@qq.com>
2023-12-12 02:52:37 +0800

    update: 从泛型中对具体类实现

---
+++ 从泛型中对具体类实现/src/main.rs
@@ -0,0 +1,16 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+impl Point<i32> {
+    fn read(&self) {
+        println!("impl Point in Kind of i32: x={}, y={}", &self.x, &self.y);
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    let p = Point { x: 1, y: 2 };
+    p.read();
+}
+// 这里使用的关键点是，从Point的值类型多种的开始，从分散的类型当中，需要铺设到比较全的实现当中。
