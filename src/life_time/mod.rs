8047bf64f4e4695c5b2f305a50a2b1ceb7b8c79a
weykon <635883092@qq.com>
2023-12-23 11:05:38 +0800

    update for rc refcell

--- src/life_time/mod.rs
+++ src/life_time/mod.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, rc::Rc};
+
 #[derive(Debug)]
 struct Foo;
 
@@ -7,11 +9,31 @@ impl Foo {
     }
     fn share(&self) {}
 }
+struct Thing {}
 
 pub fn main() {
     let mut foo = Foo;
-    let loan = foo.mutate_and_share();
     foo.share();
+    let loan = foo.mutate_and_share();
     // 上面两条代码调换位置，问题似乎是不能在同一作用域下不可变引用和可变引用出现交集调用
     println!("{:?}", loan);
+
+    let app = App {
+        a_num: 32,
+        a_thing: Rc::new(RefCell::new(Thing {})),
+    };
+
+    let a_thing = app.a_thing.borrow();
+    a_thing.do_thing(&app);
+}
+
+struct App {
+    a_num: i32,
+    a_thing: Rc<RefCell<Thing>>,
+}
+
+impl Thing {
+    fn do_thing(&self, app: &App) {
+        println!(" in thing do_thing fn saying : {}", app.a_num);
+    }
 }
