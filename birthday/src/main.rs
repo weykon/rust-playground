53cb47e09ab83117f3e63b9a4f1c60042a1e52cf
weykon <635883092@qq.com>
2024-01-29 14:21:15 +0800

    udpate

---
+++ birthday/src/main.rs
@@ -0,0 +1,35 @@
+use chrono::{Datelike, NaiveDate};
+
+struct Person {
+    birthday: String,
+    age: i32,
+}
+
+trait Birthday {
+    fn birthday(&mut self);
+}
+impl Person {
+    fn new(time: String) -> Person {
+        let birth_date = NaiveDate::parse_from_str("2000-01-01", "%Y-%m-%d").unwrap();
+        let now = chrono::Local::now().date().naive_local();
+        let age = now.year() - birth_date.year();
+
+        Person {
+            birthday: time,
+            age,
+        }
+    }
+}
+impl Birthday for Person {
+    fn birthday(&mut self) {
+        println!("Happy Birthday!");
+        self.age += 1;
+        println!("You are now {} years old.", self.age);
+    }
+}
+
+fn main() {
+    let mut me = Person::new("2000-01-01".to_string());
+
+    me.birthday();
+}
--- src/html_parser/mod.rs
+++ src/html_parser/mod.rs
@@ -1,16 +1,45 @@
+use std::str::FromStr;
 
 struct HtmlParser {
     html: String,
-    tree: Ele
+    tree: Ele,
 }
 
-struct Ele { 
+struct Ele {
     tag: String,
-    children: Vec<Box<Ele>>,
+    children: Vec<Ele>,
 }
 
+impl HtmlParser {
+    fn parse(html: String) {
+        let mut tree = Ele {
+            tag: String::from("html"),
+            children: Vec::new(),
+        };
+        let mut current = &mut tree;
 
+        use regex::Regex;
 
-mod tests { 
+        let re = Regex::new(r"(?i)<(/?)([a-z0-9]+)(?:\s*([^>]*))?>").unwrap();
 
+        
+    }
+}
+
+mod tests {
+    use crate::html_parser::HtmlParser;
+
+    const HTML: &str = r#"<html>
+        <head>
+            <title>Test</title>
+        </head>
+        <body>
+            <h1>Test</h1>
+            <p>Test</p>
+        </body>
+        </html>
+        "#;
+
+    #[test]
+    fn test_html_parser() {}
 }
